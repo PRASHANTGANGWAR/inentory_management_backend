b06c4e21d952a943cacea10e965b91fc19074489
prashant <prashantgangwar1001@gmail.com>
2022-02-24 20:52:50 +0530

    file upload

--- nestjs/nestjs-mongo/src/inventory/inventory.controller.ts
+++ nestjs/nestjs-mongo/src/inventory/inventory.controller.ts
@@ -6,10 +6,15 @@ import { Inventory } from './schemas/inventory.schema';
 import { InventoryService } from './inventory.service';
 import { FileInterceptor, MulterModule} from '@nestjs/platform-express';
 import { Seals } from './dto/fetch-inventory.dto'
+import { UploadService } from './../upload/upload.service';
 
 @Controller('inventory')
 export class InventoryController {
-  constructor(private readonly inventoryService: InventoryService) { }
+  private readonly uploadService: UploadService;
+
+  constructor(private readonly inventoryService: InventoryService, uploadService: UploadService) { 
+    this.uploadService = uploadService
+  }
 
   @Get(':inventoryId')
   async getInventoryById(@Param('inventoryId') inventoryId: string): Promise<Inventory> {
@@ -22,13 +27,32 @@ export class InventoryController {
     return this.inventoryService.getInventory();
   }
   
+
+
   @HttpCode(200)
   @Post('inwarding')
-  async createInventory(@Body() inwardInventoryDto: ReqInwardingInventoryDto): Promise<Inventory> {
+  @UseInterceptors(FileInterceptor('file'))
+  async createInventory(@UploadedFile() file, @Body() inwardInventoryDto: ReqInwardingInventoryDto): Promise<any> {
     console.log(inwardInventoryDto, "inwardInventoryDto")
+    let UploadedFile = await this.uploadService.uploadFile(file.buffer, file.filename, file.originalname, "upload/get-file", file.length, file.originalname.split('.').pop());
+    if(UploadedFile){
+      console.log('uploaded successfully', UploadedFile)
+    } else {
+      return { message: 'File Uploaded failed ', success: false };
+    }
     // { clientId: 1, currentSealId: 'test', weight: '100', noOfCoin: '100' } createInventoryDto
-    return this.inventoryService.createInventory(inwardInventoryDto.clientId, inwardInventoryDto.currentSealId, inwardInventoryDto.currentSealId, inwardInventoryDto.noOfCoin, inwardInventoryDto.noOfCoin, inwardInventoryDto.documentPath)
+    return this.inventoryService.createInventory(inwardInventoryDto.clientId, inwardInventoryDto.currentSealId, inwardInventoryDto.currentSealId, inwardInventoryDto.noOfCoin, inwardInventoryDto.noOfCoin, UploadedFile.key + UploadedFile.extension)
   }
+
+
+  // @HttpCode(200)
+  // @Post('outwarding')
+  // async outwardInventory(@Body() inwardInventoryDto: ReqInwardingInventoryDto): Promise<any> {
+  //   console.log(inwardInventoryDto, "inwardInventoryDto")
+
+  //   // { clientId: 1, currentSealId: 'test', weight: '100', noOfCoin: '100' } createInventoryDto
+  //   return this.inventoryService.createInventory(inwardInventoryDto.clientId, inwardInventoryDto.currentSealId, inwardInventoryDto.currentSealId, inwardInventoryDto.noOfCoin, inwardInventoryDto.noOfCoin, documentPath:  UploadedFile.key )
+  // }
 
   @Patch(':inventoryId')
   async updateInventory(@Param('inventoryId') inventoryId: string, @Body() UpdateInventoryDto: UpdateInventoryDto): Promise<Inventory> {
@@ -48,11 +72,6 @@ export class InventoryController {
     return this.inventoryService.getAllSeals(clientId);
   }
 
-  @Post('upload')
-  @UseInterceptors(FileInterceptor('file', {
-    dest: './files'
-  })
-  )
 
   @Post('upload')
   @UseInterceptors(FileInterceptor('file', {
--- nestjs/nestjs-mongo/src/inventory/inventory.module.ts
+++ nestjs/nestjs-mongo/src/inventory/inventory.module.ts
@@ -4,10 +4,18 @@ import { Inventory, InventorySchema } from "./schemas/inventory.schema";
 import { InventoryService } from './inventory.service';
 import { InventoryController } from './inventory.controller';
 import { InventoryRepository } from "./inventory.repository";
+import { UploadController } from 'src/upload/upload.controller';
+import { UploadService } from 'src/upload/upload.service';
+import { UploadRepository } from 'src/upload/upload.repository';
+import { MulterModule } from '@nestjs/platform-express';
+import { UploadModule } from 'src/upload/upload.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Inventory.name, schema: InventorySchema }])],
-  controllers: [InventoryController],
+  imports: [ MulterModule.register({
+    dest: './upload',
+  }),MongooseModule.forFeature([{ name: Inventory.name, schema: InventorySchema }]), UploadModule],
+  controllers: [InventoryController, UploadController],
+
   providers: [InventoryService, InventoryRepository ]
 })
 export class InventoryModule {}
